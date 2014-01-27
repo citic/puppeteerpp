dbff2c6205eae359229a88a33c15bb0dcad65b2f
Jeisson Hidalgo <jeissonh@gmail.com>
2014-01-27 12:27:12 -0600

    Updated iOS and OS X projects to Cocos2d v3beta

--- proj.ios_mac/ios/AppController.h
+++ proj.ios_mac/ios/AppController.h
@@ -3,8 +3,8 @@
 @class RootViewController;
 
 @interface AppController : NSObject <UIApplicationDelegate> {
-	UIWindow *window;
-	RootViewController	*viewController;
+    UIWindow *window;
+    RootViewController    *viewController;
 }
 
 @end
