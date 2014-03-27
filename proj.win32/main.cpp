8dc06f18a590d2152225c058b66c9088cfee66a7
jeissonh <jeissonh@gmail.com>
2014-03-27 09:30:01 -0600

    All projects updated to Cocos2d-x v3 release candidate

--- proj.win32/main.cpp
+++ proj.win32/main.cpp
@@ -1,21 +1,18 @@
 #include "main.h"
 #include "AppDelegate.h"
-#include "CCEGLView.h"
+#include "cocos2d.h"
 
 USING_NS_CC;
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
-					   HINSTANCE hPrevInstance,
-					   LPTSTR	lpCmdLine,
-					   int	   nCmdShow)
+                       HINSTANCE hPrevInstance,
+                       LPTSTR    lpCmdLine,
+                       int       nCmdShow)
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
-	// create the application instance
-	AppDelegate app;
-	EGLView* eglView = EGLView::getInstance();
-	eglView->setViewName("puppeteerpp");
-	eglView->setFrameSize(480, 320);
-	return Application::getInstance()->run();
+    // create the application instance
+    AppDelegate app;
+    return Application::getInstance()->run();
 }
